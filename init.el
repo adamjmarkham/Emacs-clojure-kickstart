99b0626bfd1244f3afdd2e36779473bc60e8fe05
Adam Markham <adamjmarkham@Adams-MacBook-Pro.local>
2012-06-25 00:07:02 +0100

    Edited to remove error on compiling

--- init.el
+++ init.el
@@ -6,9 +6,14 @@
 
 ;; Add Marmalade package archive for Emacs starter kit and other Emacs packages
 
+;; Add Marmalade package archive for Emacs starter kit and other Emacs packages
+
+(setq mac-option-modifier nil)
+(setq mac-function-modifier 'meta)
+
 (require 'package)
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") )
+	'("marmalade" . "http://marmalade-repo.org/packages/") )
 (package-initialize)
 
 
@@ -23,13 +28,7 @@
 	clojure-mode clojure-test-mode
         rainbow-delimiters
         ac-slime
-	markdown-mode
-        popup
-        undo-tree ))
-
-;;; auto-complete only seems to work as a manual install, however that
-;;; manual install relies on popup being available
- 
+	markdown-mode ))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
@@ -40,30 +39,10 @@
 
 ;; Launch the Clojure repl via Leiningen - M-x clojure-jack-in 
 ;; Global shortcut definition to fire up clojure repl and connect to it
+
 (global-set-key (kbd "C-c C-j") 'clojure-jack-in)
 
-;; Colour match parens and other structure characters to make code easy to follow
+
+;; Colour mach parens and other structure characters to make code easy to follow
+
 (global-rainbow-delimiters-mode)
-
-;;; Enable undo-tree for everything, so you can M - _ to redo
-(global-undo-tree-mode)
-
-
-;;; Add configuration for the auto-completion package
-
-; not sure if this loadpath is required
-;; (add-to-list 'load-path "~/.emacs.d")
-
-; (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
-
-;;; not sure if require 'autocomplete is needed
-;;; (require 'autocomplete)
-
-;(require 'auto-complete-config)
-;(ac-config-default)
-
-
-(add-to-list 'load-path "~/.emacs.d/")
-(require 'auto-complete-config)
-(ac-config-default)
-
